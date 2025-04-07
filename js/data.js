969cd892862a5e5f5f0001049b522ba648617eff
Rashidbek <naimjonov@gmail.com>
2025-04-07 19:12:38 +0300

    vazifa

--- js/data.js
+++ js/data.js
@@ -1,5 +1,10 @@
 export default [
   {
+    title: "Example 3",
+    github: "https://github.com/naimjonov",
+    vercel: "https://vercel.com/new?onboarding=true",
+  },
+  {
     title: "Example 2",
     github: "https://github.com/naimjonov",
     vercel: "https://vercel.com/new?onboarding=true",
