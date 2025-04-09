3a468a757092ffc282b35f71213f47f4eb4990e3
Rashidbek <naimjonov@gmail.com>
2025-04-09 16:47:07 +0300

    .

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 export default [
   {
-    title: "figma3",
+    title: "intro-section-with-dropdown-navigation",
     github: "https://github.com/naimjonov/figm3",
     vercel: "https://figm3.vercel.app/",
   },
