cce7aee556bb6c21a7b3d4a65149bc6e29b5bdc7
Rashidbek <naimjonov@gmail.com>
2025-04-11 20:16:06 +0300

    .

--- js/data.js
+++ js/data.js
@@ -1,5 +1,10 @@
 export default [
   {
+    title: "watch",
+    github: "https://github.com/naimjonov/watch",
+    vercel: "https://watch-chi.vercel.app/",
+  },
+  {
     title: "intro-section-with-dropdown-navigation",
     github: "https://github.com/naimjonov/figm3",
     vercel: "https://figm3.vercel.app/",
