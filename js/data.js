f04ba9a77f08b8d2c289b529c72ab69dbf2e17ad
Rashidbek <naimjonov@gmail.com>
2025-04-16 14:32:36 +0300

    .

--- js/data.js
+++ js/data.js
@@ -1,9 +1,15 @@
 export default [
   {
+    title: "bacround-video",
+    github: "https://github.com/naimjonov/10-dars",
+    vercel: "https://10-dars-two-nu.vercel.app/",
+  },
+  {
     title: "watch",
     github: "https://github.com/naimjonov/watch",
     vercel: "https://watch-chi.vercel.app/",
   },
+  
   {
     title: "intro-section-with-dropdown-navigation",
     github: "https://github.com/naimjonov/figm3",
