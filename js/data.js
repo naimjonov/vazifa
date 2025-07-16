dc499cefa83fbdc9ae7c777b3e43bf43ba43f84d
Rashidbek <naimjonov@gmail.com>
2025-07-16 11:31:44 +0300

    .

--- js/data.js
+++ js/data.js
@@ -1,5 +1,10 @@
 export default [
   {
+    title: "5 oy 10 dars",
+    github: "https://github.com/naimjonov/10-vazifa",
+    vercel: "https://10-vazifa-indol.vercel.app/",
+  },
+  {
     title: "5 oy 9 dars",
     github: "https://github.com/naimjonov/5-oy-9dars",
     vercel: "https://5-oy-9dars-sand.vercel.app/",
